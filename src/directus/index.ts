5a12ad1d0eebda50ec52b7223b11c616548f661e
Judah Sullivan <159483985+judahbsullivan@users.noreply.github.com>
2024-11-21 20:27:04 -0600

    🔌Directus-CMS to Frontend Routing
    Tied the main pages and rendering on the frontend

--- src/directus/index.ts
+++ src/directus/index.ts
@@ -1,9 +1,18 @@
-import { createDirectus, rest } from "@directus/sdk";
+import { createDirectus, graphql, readItems, rest } from "@directus/sdk";
 import type { DirectusInterface } from "./types";
 import { db } from "@/utils/env";
 
-export const directusClient = createDirectus<DirectusInterface>(`${db}`).with(rest());
+export const directusClient = createDirectus<DirectusInterface>(db)
+  .with(rest())
+  .with(graphql());
 
-
-
-
+export async function getPagesBySlug() {
+  const pages = await directusClient.request(
+    // @ts-expect-error Typings should include this
+    readItems("pages", {
+      fields: ["*"],
+    }),
+  );
+  console.log(pages);
+  return pages;
+}
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,2 +1,4 @@
-export const isDev = import.meta.env.DEV;
-export const db = import.meta.env.DIRECTUS_DATABASE_URL || ""
+export const isDev = import.meta.env.DEV
+  ? import.meta.env.DEV
+  : import.meta.env.PROD;
+export const db = import.meta.env.DIRECTUS_DATABASE_URL;
